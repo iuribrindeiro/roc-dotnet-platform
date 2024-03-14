c97fa54b60cf4e640a941ee69c5f76a34d20e4ad
Iuri Brindeiro <ibrindeiro@autosoftdms.com>
2024-03-14 18:51:36 -0300

    comments to answer my own questions in the future

--- platform/Platform.cs
+++ platform/Platform.cs
@@ -8,11 +8,16 @@ public static partial class Platform
 {
     public static unsafe string RocStrRead(RocStr rocStr)
     {
+        //not sure why, but when converting from UIntPtr to long, it becomes negative
         if ((long)rocStr.capacity < 0)
         {
             // Small string
             int byteLen = IntPtr.Size == 8 ? 24 : 12;
             byte[] bytes = new byte[byteLen];
+            //getting the actual reference to the rocStr, converting it to a IntPtr
+            //and then copying its byte array content to `bytes` seems to work,
+            //but using the actual instance .bytes doesnt.
+            //I still need to understand what is the .bytes and why is it differnet from (IntPtr)(&rocStr)
             Marshal.Copy((IntPtr)(&rocStr), bytes, 0, byteLen);
             int lenSmall = bytes[byteLen - 1] ^ 128;
             return Encoding.ASCII.GetString(bytes, 0, lenSmall);
